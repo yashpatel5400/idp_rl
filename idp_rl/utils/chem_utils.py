5b788ae9f229c439219f69657bbe78b20e9e6a31
Yash Patel <ypatel5400@gmail.com>
2022-06-21 14:13:58 -0400

    Add basic CHARMM scaffolding to pipeline (untested)

--- idp_rl/utils/chem_utils.py
+++ idp_rl/utils/chem_utils.py
@@ -17,17 +17,61 @@ import logging
 import enum
 import torch
 
+class ForceField(enum.Enum):
+    MMFF = "MMFF"
+    CHARMM = "CHARMM"
+
+charmm_simulator = None
+
+def seed_charmm_simulator(psf_fn):
+    global charmm_simulator
+    if charmm_simulator is not None:
+        return
+
+    toppar_filenames = ["toppar/par_all36_prot.prm", "toppar/top_all36_prot.rtf","toppar/toppar_water_ions.str"]
+
+    openmm_toppar = app.CharmmParameterSet(*toppar_filenames)
+    openmm_psf = app.CharmmPsfFile(psf_fn)
+    openmm_system = openmm_psf.createSystem(openmm_toppar, **system_kwargs)
+
+    # TODO: test GPU version simulator
+    integrator = openmm.VerletIntegrator(1.0)
+
+    if torch.cuda.is_available():
+        platform = openmm.Platform.getPlatformByName("CUDA")
+        prop = dict(CudaPrecision="mixed")
+        charmm_simulator = app.Simulation(psf.topology, system, integrator, platform, prop)
+    elif platform_type == "GPU":
+        platform = openmm.Platform.getPlatformByName()
+        charmm_simulator = app.Simulation(openmm_psf.topology, openmm_system, integrator, platform)
+    
+def charmm_energy(psf_fn, positions):
+    seed_charmm_simulator(psf_fn)
+    global charmm_simulator
+    charmm_simulator.context.setPositions(positions)
+    energy = simulation.context.getState(getEnergy=True).getPotentialEnergy()
+    return energy
+
+def charmm_optimize_conf(psf_fn, positions):
+    seed_charmm_simulator(psf_fn)
+    global charmm_simulator
+    charmm_simulator.context.setPositions(positions)
+    simulation.minimizeEnergy(maxIterations=500)
+    optimized_positions = simulation.context.getState(getPositions=True).getPositions()
+    return optimized_positions
+
 def get_conformer_energies(mol: Chem.Mol, ff: ForceField=ForceField.MMFF) -> List[float]:
     """Returns a list of energies for each conformer in `mol`.
     """
     energies = []
 
-    Chem.MMFFSanitizeMolecule(mol)
-    mmff_props = Chem.MMFFGetMoleculeProperties(mol)
-    for conf in mol.GetConformers():
-        ff = Chem.MMFFGetMoleculeForceField(mol, mmff_props, confId=conf.GetId())
-        energy = ff.CalcEnergy()
-        energies.append(energy)
+    if ff == ForceField.MMFF:
+        Chem.MMFFSanitizeMolecule(mol)
+        mmff_props = Chem.MMFFGetMoleculeProperties(mol)
+        for conf in mol.GetConformers():
+            ff = Chem.MMFFGetMoleculeForceField(mol, mmff_props, confId=conf.GetId())
+            energy = ff.CalcEnergy()
+            energies.append(energy)
     return np.asarray(energies, dtype=float)
 
 def get_conformer_energy(mol: Chem.Mol, confId: int = None) -> float:
