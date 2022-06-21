14e6e82dfab6d629f5bf7f25ab950e47e4bd3592
Yash Patel <ypatel5400@gmail.com>
2022-06-21 14:17:55 -0400

    Separate out CHARMM utils

--- idp_rl/utils/chem_utils.py
+++ idp_rl/utils/chem_utils.py
@@ -8,57 +8,14 @@ import numpy as np
 import bisect
 from rdkit.Chem import TorsionFingerprints
 import rdkit.Chem.AllChem as Chem
-import openmm
-import openmm.app as app
-import openmm.unit as u
 
 from typing import Tuple, List
 import logging
 import enum
-import torch
 
 class ForceField(enum.Enum):
     MMFF = "MMFF"
     CHARMM = "CHARMM"
-
-charmm_simulator = None
-
-def seed_charmm_simulator(psf_fn):
-    global charmm_simulator
-    if charmm_simulator is not None:
-        return
-
-    toppar_filenames = ["toppar/par_all36_prot.prm", "toppar/top_all36_prot.rtf","toppar/toppar_water_ions.str"]
-
-    openmm_toppar = app.CharmmParameterSet(*toppar_filenames)
-    openmm_psf = app.CharmmPsfFile(psf_fn)
-    openmm_system = openmm_psf.createSystem(openmm_toppar, **system_kwargs)
-
-    # TODO: test GPU version simulator
-    integrator = openmm.VerletIntegrator(1.0)
-
-    if torch.cuda.is_available():
-        platform = openmm.Platform.getPlatformByName("CUDA")
-        prop = dict(CudaPrecision="mixed")
-        charmm_simulator = app.Simulation(psf.topology, system, integrator, platform, prop)
-    elif platform_type == "GPU":
-        platform = openmm.Platform.getPlatformByName()
-        charmm_simulator = app.Simulation(openmm_psf.topology, openmm_system, integrator, platform)
-    
-def charmm_energy(psf_fn, positions):
-    seed_charmm_simulator(psf_fn)
-    global charmm_simulator
-    charmm_simulator.context.setPositions(positions)
-    energy = simulation.context.getState(getEnergy=True).getPotentialEnergy()
-    return energy
-
-def charmm_optimize_conf(psf_fn, positions):
-    seed_charmm_simulator(psf_fn)
-    global charmm_simulator
-    charmm_simulator.context.setPositions(positions)
-    simulation.minimizeEnergy(maxIterations=500)
-    optimized_positions = simulation.context.getState(getPositions=True).getPositions()
-    return optimized_positions
 
 def get_conformer_energies(mol: Chem.Mol, ff: ForceField=ForceField.MMFF) -> List[float]:
     """Returns a list of energies for each conformer in `mol`.
---
+++ tests/utils/test_charmm_utils.py
@@ -0,0 +1,5 @@
+from idp_rl.utils import charmm_utils
+import numpy as np
+
+def test_charmm_sim(mocker):
+    pass
