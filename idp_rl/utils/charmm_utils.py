cacbb7b83b2351b47d8b9d5ce64ecde76de92d4b
Yash Patel <ypatel5400@gmail.com>
2022-06-21 15:20:16 -0400

    Add basic CHARMM optimization test

--- idp_rl/utils/charmm_utils.py
+++ idp_rl/utils/charmm_utils.py
@@ -33,24 +33,18 @@ def seed_charmm_simulator(psf_fn: str, toppar_filenames: List[str]):
     else:
         platform = openmm.Platform.getPlatformByName("CPU")
         charmm_simulator = app.Simulation(openmm_psf.topology, openmm_system, integrator, platform)
-    
-def np_to_mm(arr: np.ndarray, unit: openmm.unit=openmm.unit.angstrom):
-    wrapped_val = openmm.unit.quantity.Quantity(arr, unit)
-    return wrapped_val
 
-def charmm_energy(psf_fn: str, toppar_filenames: List[str], positions: np.ndarray):
+def charmm_energy(psf_fn: str, toppar_filenames: List[str], positions: openmm.unit.quantity.Quantity):
     seed_charmm_simulator(psf_fn, toppar_filenames)
     global charmm_simulator
-    mm_pos = np_to_mm(positions)
-    charmm_simulator.context.setPositions(mm_pos)
+    charmm_simulator.context.setPositions(positions)
     energy = charmm_simulator.context.getState(getEnergy=True).getPotentialEnergy()
     return energy
 
-def charmm_optimize_conf(psf_fn: str, toppar_filenames: List[str], positions: np.ndarray):
+def charmm_optimize_conf(psf_fn: str, toppar_filenames: List[str], positions: openmm.unit.quantity.Quantity):
     seed_charmm_simulator(psf_fn, toppar_filenames)
     global charmm_simulator
-    mm_pos = np_to_mm(positions)
-    charmm_simulator.context.setPositions(mm_pos)
+    charmm_simulator.context.setPositions(positions)
     charmm_simulator.minimizeEnergy(maxIterations=500)
     optimized_positions = charmm_simulator.context.getState(getPositions=True).getPositions()
     return optimized_positions
--- tests/utils/test_charmm_utils.py
+++ tests/utils/test_charmm_utils.py
@@ -1,6 +1,7 @@
 from idp_rl.utils import charmm_utils
 
 import numpy as np
+import openmm
 import parmed as pmd
 import rdkit.Chem.AllChem as Chem
 
@@ -11,6 +12,10 @@ toppar_filenames = [
     "idp_rl/utils/toppar/toppar_water_ions.str"
 ]
 
+def np_to_mm(arr: np.ndarray, unit: openmm.unit=openmm.unit.angstrom):
+    wrapped_val = openmm.unit.quantity.Quantity(arr, unit)
+    return wrapped_val
+
 def test_seed_charmm(mocker):
     charmm_utils.seed_charmm_simulator(chignolin_psf_fn, toppar_filenames)
 
@@ -20,11 +25,28 @@ def test_charmm_energy(mocker):
     # test from RDKit and compare with ParmEd to ensure no issue with reading
     chignolin = Chem.rdmolfiles.MolFromPDBFile(chignolin_pdb_fn, removeHs=False)
     conf = chignolin.GetConformer(0)
-    positions = conf.GetPositions()
+    positions = np_to_mm(conf.GetPositions())
     chignolin_energy = charmm_utils.charmm_energy(chignolin_psf_fn, toppar_filenames, positions)
 
     pmd_chignolin = pmd.load_file(chignolin_pdb_fn)
-    pmd_np_pos = pmd_chignolin.positions._value
+    pmd_np_pos = pmd_chignolin.positions
     pmd_chignolin_energy = charmm_utils.charmm_energy(chignolin_psf_fn, toppar_filenames, pmd_np_pos)
 
+    assert(np.isclose(chignolin_energy._value, 1881.096))
     assert(np.isclose(chignolin_energy._value, pmd_chignolin_energy._value))
+
+def test_charmm_opt(mocker):
+    chignolin_pdb_fn = "idp_rl/molecule_generation/chignolin/1uao.pdb"
+    
+    # test from RDKit and compare with ParmEd to ensure no issue with reading
+    chignolin = Chem.rdmolfiles.MolFromPDBFile(chignolin_pdb_fn, removeHs=False)
+    conf = chignolin.GetConformer(0)
+    positions = np_to_mm(conf.GetPositions())
+    optimized_pos = charmm_utils.charmm_optimize_conf(chignolin_psf_fn, toppar_filenames, positions)
+    
+    pre_chignolin_energy = charmm_utils.charmm_energy(chignolin_psf_fn, toppar_filenames, positions)
+    opt_chignolin_energy = charmm_utils.charmm_energy(chignolin_psf_fn, toppar_filenames, optimized_pos)
+
+    assert(opt_chignolin_energy._value < pre_chignolin_energy._value)    
+    opt_thresh = 65 # optimization should minimize to a value < 65 (usually between 50-60)
+    assert(opt_chignolin_energy._value < opt_thresh)
